acf9b1f7e22351cf2bb7621c45ef5360781066bb
Uurtsaikh Batbileg <uurtsaikh.ba@gmail.com>
2017-08-23 23:17:27 -0700

    more;

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,13 +3,13 @@ import React from 'react'
 import { Text, View } from 'react-native';
 
 // make a component
-const Header = (props) => {
+const Header = ({ headerText }) => {
     const { textStyle, viewStyle } = styles;
 
     return (
         <View style = { viewStyle }>
             <Text style = { textStyle }>
-                { props.headerText}
+                { headerText}
             </Text>
         </View>
     );
@@ -17,20 +17,18 @@ const Header = (props) => {
 
 const styles = {
     viewStyle: {
-        backgroundColor: '#F8F8F8',
+        backgroundColor: '#009af8',
         height: 60,
         justifyContent: 'center',
         alignItems: 'center',
         paddingTop: 15,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 1 },
-        shadowOpacity: 0.2,
         elevation: 2,
         position: 'relative'
     },
     textStyle: {
         fontSize: 20,
-        color: 'gray'
+        fontWeight: 'bold',
+        color: 'white'
     }
 };
 
